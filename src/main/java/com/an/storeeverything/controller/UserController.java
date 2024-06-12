03f54fa55527e7deefebb6beaeb2456079fe6943
Andrzej <t4r4boom@gmail.com>
2024-06-12 19:26:34 +0200

    w sumie nic nie zrobilem xd

--- src/main/java/com/an/storeeverything/controller/UserController.java
+++ src/main/java/com/an/storeeverything/controller/UserController.java
@@ -10,6 +10,7 @@ import com.an.storeeverything.models.UserEntity;
 import com.an.storeeverything.models.RolesEntity;
 import com.an.storeeverything.services.UserService;
 import com.an.storeeverything.repository.RolesRepository;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -43,9 +44,34 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+
+        model.addAttribute("user", new UserEntity());
+
         return "login";
     }
+
+    @PostMapping("/login-user")
+    public String loginUser(@ModelAttribute UserEntity user, Model model) {
+        var currentUser = userService.findByUsername(user.getUsername());
+
+        System.out.println(user);
+        System.out.println(currentUser);
+
+
+        if(currentUser == null) {
+            System.out.println("User not found");
+            return "login";
+        }
+
+        if(currentUser.getPassword().equals(user.getPassword())) {
+            model.addAttribute("user", user);
+            return "redirect:/home";
+        } else return "login";
+
+
+    }
+
 
     @GetMapping("/home")
     public String home() {
